8e9ab8b193a0904b0eb3140625c47cd3732701fd
Hakan Bilen <hbilen@robots.ox.ac.uk>
2017-06-04 16:29:51 +0100

    Rm ga

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-rsync -avx _site/index.html hbilen@robots.ox.ac.uk:/homes/45/hbilen/WWW/ --delete
-rsync -avx _site/publications/ hbilen@robots.ox.ac.uk:/homes/45/hbilen/WWW/publications/ --delete
-rsync -avx _site/assets/ hbilen@robots.ox.ac.uk:/homes/45/hbilen/WWW/assets/ --delete
+rsync -avx _site/index.html hbilen@robots.ox.ac.uk:/homes/33/hbilen/WWW/ --delete
+rsync -avx _site/publications/ hbilen@robots.ox.ac.uk:/homes/33/hbilen/WWW/publications/ --delete
+rsync -avx _site/assets/ hbilen@robots.ox.ac.uk:/homes/33/hbilen/WWW/assets/ --delete
