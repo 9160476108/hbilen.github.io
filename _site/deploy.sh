151a46b7c82eb95c454727bc361852fc786ed0ca
Hakan Bilen <hbilen@robots.ox.ac.uk>
2017-10-01 09:52:08 +0100

    Update deploy

--- _site/deploy.sh
+++ _site/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 rsync -avx _site/index.html    hbilen@staff.ssh.inf.ed.ac.uk:/public/homepages/hbilen/web/ --delete
 rsync -avx _site/publications/ hbilen@staff.ssh.inf.ed.ac.uk:/public/homepages/hbilen/web/publications/ --delete
+rsync -avx _site/vacancies/ hbilen@staff.ssh.inf.ed.ac.uk:/public/homepages/hbilen/web/vacancies/ --delete
 rsync -avx _site/assets/       hbilen@staff.ssh.inf.ed.ac.uk:/public/homepages/hbilen/web/assets/ --delete
